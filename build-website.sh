37a22f2f0a8e761891e53b9e84f60c77a60d43da
harry.cpp <harry.cpp@outlook.com>
2023-02-04 14:04:50 +0100

    Delete debug banlist during banlist generation

--- build-website.sh
+++ build-website.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf GXFormat.Website/wwwroot/data/GXFormat_2099.01.01.lflist.conf
 dotnet build -c Release GXFormat.Generator
 dotnet run -c Release --project GXFormat.Generator/GXFormat.Generator.csproj
 dotnet publish -c Release GXFormat.Website/GXFormat.Website.csproj
